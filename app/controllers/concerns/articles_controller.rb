8a99445cfdbf44a8d37c2d3b727c9c0ffc1726de
Charlotte Udemy Course <charlotte.taylor@deliveroo.co.uk>
2018-12-30 18:39:20 +0000

    style errors and messages partials

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -21,11 +21,8 @@ class ArticlesController < ApplicationController
   def create
 
     @article = Article.new(article_params)
-
     if @article.save
-
-      flash[:notice] = "Article was successfully created"
-
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
 
     else
@@ -37,17 +34,11 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
     if @article.update(article_params)
-
-      flash[:notice] = "Article was successfully updated"
-
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
-
     else
-
       render 'edit'
-
     end
 
   end
@@ -59,11 +50,8 @@ class ArticlesController < ApplicationController
   def destroy
 
     @article.destroy
-
-    flash[:notice] = "Article was successfully deleted"
-
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
-
   end
 
   private
